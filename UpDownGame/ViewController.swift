8d91b03adcb57020db01dd7cf067be1f5f146b15
ljh97820 <ljh97820@naver.com>
2021-07-08 18:58:53 +0900

    First Commit

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slider.minimumValue = 0
+        slider.maximumValue = 30
+        slider.value = 15
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        print("Touch Up Reset Button")
+        slider.value = 15
+    }
 }
 
