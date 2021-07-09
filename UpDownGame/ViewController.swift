b26f5f5e84df0dc382dee0eed4ec22c3dce7db9c
ljh97820 <ljh97820@naver.com>
2021-07-09 23:14:16 +0900

    두 번째 커밋. Auto Layout, RandomValue, MinimumValue, MaximumValue, 현재값 가져오기

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,10 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue : Int = 0
+    var tryCount : Int = 0
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
         
         slider.minimumValue = 0
         slider.maximumValue = 30
@@ -22,15 +31,37 @@ class ViewController: UIViewController {
 
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
+        let integerValue: Int = Int(sender.value)
+        sliderValueLabel.text = String(integerValue)
     }
 
     @IBAction func touchUpHitButton(_ sender: UIButton) {
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount = tryCount + 1
+        tryCountLabel.text = "\(tryCount) / 5"
+        
     }
     
     @IBAction func touchUpResetButton(_ sender: UIButton) {
         print("Touch Up Reset Button")
+        reset()
+    }
+    
+    func reset() {
+        print("Reset!")
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0 / 5"
+        slider.minimumValue = 0
+        slider.maximumValue = 30
         slider.value = 15
+        minimumValueLabel.text = "0"
+        maximumValueLabel.text = "30"
+        sliderValueLabel.text = "15"
     }
 }
 
